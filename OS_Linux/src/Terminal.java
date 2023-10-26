b26c79b25d95761dc96be3211b5cb05a8b4cad15
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2023-10-26 12:01:08 +0300

    added (parse) method's implementation and the basic implementation of the main function

--- OS_Linux/src/Terminal.java
+++ OS_Linux/src/Terminal.java
@@ -1,11 +1,27 @@
 package OS_Linux.src;
 
+import java.util.Scanner;
+
 class Parser{
     String commandName;
     String[] args;
     //This method will divide the input into commandName and args
     //where "input" is the string command entered by the user
     public boolean parse(String input){ //returns true if the command is valid
+     // input is the command
+        commandName = input;
+        // args should be the arguments of the command
+        // for example, if the command is "echo hello world", then commandName = "echo" and args = ["hello", "world"]
+        // if the command is "pwd", then commandName = "pwd" and args = []
+        args = new String[0];
+        if(input.contains(" ")){ // if the command contains space
+            String[] temp = input.split(" "); // split the command by space
+            commandName = temp[0]; // the first word is the command name
+            args = new String[temp.length-1]; // the remaining words are the arguments
+            for(int i=1;i<temp.length;i++){
+                args[i-1] = temp[i];
+            }
+        }
         return true;
     }
     public String getCommandName(){ //returns the command name
@@ -22,8 +38,36 @@ public class Terminal{
     public Terminal(){
         parser = new Parser();
     }
+    public void echo(String[] args){ // echo command in linux prints the arguments
+        for(int i=0;i<args.length;i++){
+            System.out.print(args[i]+" "); //print the arguments
+        }
+        System.out.println();
+    }
     public static void main(String[] args){
-        System.out.println("Hello World");
+        while(true){
+            System.out.print(">> ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if(input.equals("exit")){ // if the user enters exit, then exit the program
+                break;
+            }
+            Parser parser = new Parser();
+            if(parser.parse(input)){ // if the command is valid
+                String commandName = parser.getCommandName();
+                String[] arguments = parser.getArgs();
+                if(commandName.equals("echo")){ // if the command is echo
+                    Terminal terminal = new Terminal();
+                    terminal.echo(arguments);
+                }
+                else{
+                    System.out.println("Invalid command");
+                }
+            }
+            else{
+                System.out.println("Invalid command");
+            }
+        }
 
     }
 
